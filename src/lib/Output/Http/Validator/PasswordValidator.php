37f20d82a50965690dbdac79b9caa84eb7a5fc5c
SeaLife <mircotries@hotmail.de>
2018-10-08 01:24:33 +0200

    Improved Validating (password)

--- src/lib/Output/Http/Validator/PasswordValidator.php
+++ src/lib/Output/Http/Validator/PasswordValidator.php
@@ -4,60 +4,45 @@ namespace Output\Http\Validator;
 
 use Globals\Exception\ValidationException;
 use Output\Http\Form;
+use Output\Http\FormFieldValidator;
 
-class PasswordValidator extends ChainValidator {
+class PasswordValidator implements FormFieldValidator {
+
+    private $factory = NULL;
+    private $level   = 0;
+
     public function __construct ($level = 0) {
-        parent::__construct();
 
-        if ($level > 0) {
-            $this->add(new StringContainsValidator("[a-z]", FALSE, "Password must contain lower case characters."));
+        $this->factory = ValidatorFactory::create();
+        $this->level   = $level;
+
+        $this->factory->isSmallerThan(255); # just to make sure the password can fit the database? :D
+
+        if ($level >= 0) {
+            $this->factory
+                ->contains("[a-z]", FALSE, 'Must at least contain 1 lower character')
+                ->contains("[A-Z]", FALSE, 'Must at least contain 1 upper character')
+                ->isLongerThan($this->level <= 4 ? 4 : 8);
         }
 
-        if ($level > 1) {
-            $this->add(new StringContainsValidator("[A-Z]", FALSE, "Password must contain upper case characters."));
+        if ($level >= 1) {
+            $this->factory->contains('[0-9]', FALSE, 'Must at least contain 1 digit');
         }
 
-        if ($level > 2) {
-            $this->add(new StringContainsValidator("[0-9]", FALSE, "Password must contain at least 1 numeric"));
+        if ($level >= 2) {
+            $this->factory->contains("[\'^£$%&*()}{@#~?!§><>\/\\\,|=_+¬-]", FALSE, 'Must at least contain 1 special character');
         }
 
-        if ($level > 3 && $level <= 6) {
-            $this->add(new StringSequenceValidator(3, TRUE));
-            $this->add(new PwnedPasswordValidator(2));
-        }
-        if ($level > 6) {
-            $this->add(new StringSequenceValidator(2, TRUE));
-            $this->add(new PwnedPasswordValidator(0));
+        if ($level >= 5) {
+            $this->factory->isNotContainedWithinThePawnedPasswordDatabase($this->level <= 7 ? 2 : 0);
         }
 
-        if ($level > 4) {
-            $this->add(new StringContainsValidator("[\'^£$%&*()}{@#~?!§><>\/\\\,|=_+¬-]", FALSE, "Password must contain at least 1 special character"));
-        }
-
-        if ($level >= 4 && $level < 9) {
-            $this->add(new StringLengthValidator(4, "Password must be at least :length: characters long."));
-        }
-
-        if ($level >= 9) {
-            $this->add(new StringLengthValidator(8, "Password must be at least :length: characters long."));
+        if ($level >= 8) {
+            $this->factory->isNotContainingACharSequenceOf(3);
         }
     }
 
     public function isValid (Form $form, $field) {
-        $messages = array();
-
-        foreach ($this->validators as $validator) {
-            try {
-                $val = $validator->isValid($form, $field);
-
-                if (!$val && is_bool($val)) return $val;
-            } catch (ValidationException $e) {
-                array_push($messages, $e->getMessage());
-            }
-        }
-
-        if (!empty($messages)) {
-            throw new ValidationException(join("<br>", $messages));
-        }
+        return $this->factory->isValid($form, $field);
     }
 }
---
+++ src/lib/Output/Http/Validator/ValidatorFactory.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Output\Http\Validator;
+
+use Output\Http\Form;
+use Output\Http\FormFieldValidator;
+
+class ValidatorFactory implements FormFieldValidator {
+
+    private $chain;
+
+    public static function create () {
+        return new ValidatorFactory();
+    }
+
+    public function __construct () {
+        $this->chain = new ChainValidator();
+    }
+
+    public function isLongerThan ($int) {
+        $this->chain->add(new StringLengthValidator($int));
+        return $this;
+    }
+
+    public function isSmallerThan ($int) {
+        $this->chain->add(new StringLengthLimitValidator($int));
+        return $this;
+    }
+
+    public function isEmail () {
+        $this->chain->add(new EmailValidator());
+        return $this;
+    }
+
+    public function isNonExistentInDatabase ($entity, $column) {
+        $this->chain->add(new HasNoDatabaseEntryValidator($entity, $column));
+        return $this;
+    }
+
+    public function isPasswordSecure ($level = 3) {
+        $this->chain->add(new PasswordValidator($level));
+        return $this;
+    }
+
+    public function isNotContainingACharSequenceOf ($length = 2) {
+        $this->chain->add(new StringSequenceValidator($length));
+        return $this;
+    }
+
+    public function contains ($regex, $onFind = TRUE, $message = "Value is not valid") {
+        $this->chain->add(new StringContainsValidator($regex, $onFind, $message));
+        return $this;
+    }
+
+    public function isNotContainedWithinThePawnedPasswordDatabase ($limit = 0) {
+        $this->chain->add(new PwnedPasswordValidator($limit));
+        return $this;
+    }
+
+    /**
+     * @param Form $form
+     * @param      $field
+     *
+     * @return bool
+     */
+    public function isValid (Form $form, $field) {
+        return $this->chain->isValid($form, $field);
+    }
+}
